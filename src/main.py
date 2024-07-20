47bf0247589a5dc00f21f7b4a6436cf321cf28bd
Soew <55360932+Lousio-n@users.noreply.github.com>
2024-07-20 17:12:06 +0200

    add htmlnode to main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from textnode import Textnode
+from htmlnode import HTMLNode
 
 
 def main():
     node = Textnode("this is a test", "bold", "google.se")
     print(node)
+    html = HTMLNode("p", "this is a test", [], {
+                    "href": "https://www.google.com"})
+    print(html.props_to_html())
 
 
 main()
