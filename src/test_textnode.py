6174687da0733e5a02f9daa4285c6d9e9b57b64f
Soew <55360932+Lousio-n@users.noreply.github.com>
2024-07-20 15:45:53 +0200

    change tests to use imported types

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,32 +1,48 @@
 import unittest
 
-from textnode import Textnode
+from textnode import (
+    Textnode,
+    text_type_bold,
+    text_type_code,
+    text_type_link,
+    text_type_text,
+    text_type_image,
+    text_type_italic,
+)
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = Textnode("this is a textnode", "bold")
-        node2 = Textnode("this is a textnode", "bold")
+        node = Textnode("this is a textnode", text_type_bold)
+        node2 = Textnode("this is a textnode", text_type_bold)
         self.assertEqual(node, node2)
 
     def test_url(self):
-        node = Textnode("this is a textnode", "link", "https://url.com")
-        node2 = Textnode("this is a textnode", "link", "https://url.com")
+        node = Textnode("this is a textnode",
+                        text_type_link, "https://url.com")
+        node2 = Textnode("this is a textnode",
+                         text_type_link, "https://url.com")
         self.assertEqual(node, node2)
 
     def test_url_none(self):
-        node = Textnode("this is a textnode", "bold")
+        node = Textnode("this is a textnode", text_type_italic)
         self.assertEqual(node.url, None)
 
     def test_not_eq_text(self):
-        node = Textnode("this is a textnode", "bold")
-        node2 = Textnode("this is different", "bold")
-        self.assertNotEquals(node.text, node2.text)
+        node = Textnode("this is a textnode", text_type_text)
+        node2 = Textnode("this is different", text_type_text)
+        self.assertNotEquals(node, node2)
 
     def test_not_eq_text_type(self):
-        node = Textnode("this is a textnode", "bold")
-        node2 = Textnode("this is a textnode", "italic")
-        self.assertNotEquals(node.text_type, node2.text_type)
+        node = Textnode("this is a textnode", text_type_bold)
+        node2 = Textnode("this is a textnode", text_type_italic)
+        self.assertNotEquals(node, node2)
+
+    def test_repr(self):
+        node = Textnode("this is a textnode",
+                        text_type_link, "https://url.com")
+        self.assertEqual(
+            "Textnode(this is a textnode, link, https://url.com)", repr(node))
 
 
 if __name__ == "__main__":
