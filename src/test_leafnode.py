878fb0b65135a43c1ec7b636a71b8220b0a2a5db
Soew <55360932+Lousio-n@users.noreply.github.com>
2024-07-21 14:43:54 +0200

    add more tests and change the import

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -1,9 +1,18 @@
 import unittest
 
-from leafnode import LeafNode
+from htmlnode import LeafNode
 
 
 class TestLeafNode(unittest.TestCase):
     def test_to_html(self):
-        node = LeafNode("a", "test", {"href": "google.com"})
-        self.assertEqual(node.to_html(), '<a href="google.com">test</a>')
+        node = LeafNode("a", "test", {"href": "google.com", "class": "link"})
+        self.assertEqual(
+            node.to_html(), '<a href="google.com" class="link">test</a>')
+
+    def test_value(self):
+        node = LeafNode("p", "this is a paragraph")
+        self.assertEqual(node.to_html(), '<p>this is a paragraph</p>')
+
+    def test_no_tag(self):
+        node = LeafNode(None, "this is raw text")
+        self.assertEqual(node.to_html(), 'this is raw text')
